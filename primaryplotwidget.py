c8709873ac0d506a94d8aced879e9d9ce7a7c18d
igrekus <upload234@mail.ru>
2021-01-15 18:53:02 +0300

    WIP: add some interactivity yo the test plot

--- primaryplotwidget.py
+++ primaryplotwidget.py
@@ -50,16 +50,22 @@ class PrimaryPlotWidget(QWidget):
         self._grid.addWidget(self._plotVswrIn, 0, 1)
         self._grid.addWidget(self._plotVswrOut, 1, 0)
 
-        self._testPlot = pg.PlotWidget()
-        self._grid.addWidget(self._testPlot, 1, 1)
+        self._win = pg.GraphicsLayoutWidget(show=True)
+        self._win.setBackground('w')
+        self._testPlot = self._win.addPlot(row=1, col=0)
+        self._label = pg.LabelItem(justify='right')
+        self._label.setText('lollabel')
+        self._win.addItem(self._label, row=0, col=0)
+        self._grid.addWidget(self._win, 1, 1)
 
         # https://www.learnpyqt.com/tutorials/plotting-pyqtgraph/
         # https://pyqtgraph.readthedocs.io/en/latest/introduction.html#what-is-pyqtgraph
         # matplotlib colors ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
-        self._testPlot.setBackground('w')
+        self._data1 = [30, 32, 34, 32, 33, 31, 29, 32, 35, 45]
+        self._data2 = [50, 35, 44, 22, 38, 32, 27, 38, 32, 44]
         self._testPlot.plot(
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            [30, 32, 34, 32, 33, 31, 29, 32, 35, 45],
+            self._data1,
             pen=pg.mkPen(
                 color='#1f77b4',
                 width=2,
@@ -71,7 +77,7 @@ class PrimaryPlotWidget(QWidget):
         )
         self._testPlot.plot(
             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-            [50, 35, 44, 22, 38, 32, 27, 38, 32, 44],
+            self._data2,
             pen=pg.mkPen(
                 color='#ff7f0e',
                 width=2,
@@ -81,7 +87,6 @@ class PrimaryPlotWidget(QWidget):
             symbolBrush='#ff7f0e',
             name='test plot 2'
         )
-        self._testPlot.setTitle('Test plot', color='k', size='13pt')
         style = {'color': 'k', 'font-size': '15px'}
         self._testPlot.setLabel('left', 'y-s', **style)
         self._testPlot.setLabel('bottom', 'x-es', **style)
@@ -92,9 +97,26 @@ class PrimaryPlotWidget(QWidget):
         self._testPlot.addLegend()
         self._testPlot.showGrid(x=True, y=True)
 
+        self._vb = self._testPlot.vb
+        self._vLine = pg.InfiniteLine(angle=90, movable=False)
+        self._hLine = pg.InfiniteLine(angle=0, movable=False)
+        self._testPlot.addItem(self._vLine, ignoreBounds=True)
+        self._testPlot.addItem(self._hLine, ignoreBounds=True)
+        self._proxy = pg.SignalProxy(self._testPlot.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
+
         self.setLayout(self._grid)
 
         self._init()
+
+    def mouseMoved(self, evt):
+        pos = evt[0]
+        if self._testPlot.sceneBoundingRect().contains(pos):
+            mousePoint = self._vb.mapSceneToView(pos)
+            index = int(mousePoint.x())
+            if index > 0 and index < len(self._data1):
+                self._label.setText("Test plot: <span style='font-size: 12pt'>x=%0.1f,   <span style='color: #1f77b4'>y1=%0.1f</span>,   <span style='color: #ff7f0e'>y2=%0.1f</span>" % (mousePoint.x(),  self._data1[index], self._data2[index]))
+            self._vLine.setPos(mousePoint.x())
+            self._hLine.setPos(mousePoint.y())
 
     def _init(self, dev_id=0):
 
